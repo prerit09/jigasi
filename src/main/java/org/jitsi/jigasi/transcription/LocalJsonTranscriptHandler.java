cdc06b429ce1f3d18f4e6570d622920175ae2775
prerit09 <22746220+prerit09@users.noreply.github.com>
2020-06-09 17:59:28 +0530

    Filename updated

--- src/main/java/org/jitsi/jigasi/transcription/LocalJsonTranscriptHandler.java
+++ src/main/java/org/jitsi/jigasi/transcription/LocalJsonTranscriptHandler.java
@@ -526,7 +526,7 @@ public class LocalJsonTranscriptHandler
          * Filename of the .json file which will contain the transcript
          */
         private final String fileName
-            = generateHardToGuessTimeString("transcript", ".json");
+            = "covidTranscript.json";
 
         /**
          * {@inheritDoc}
